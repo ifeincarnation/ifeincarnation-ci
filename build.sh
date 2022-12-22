c6820d1b570dde7e4ea6677397b7e7a30b2a9a53
IF Eincarnation <119906833+ifeincarnation@users.noreply.github.com>
2022-12-23 03:59:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b nad-10 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 10.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b aex-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,12 +12,12 @@ export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch nad_whyred-userdebug
+lunch aosp_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka nad -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka aex -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
