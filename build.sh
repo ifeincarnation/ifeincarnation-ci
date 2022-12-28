4dcf195117ffb068ec0dc239bf38bc11d05885e7
IF Eincarnation <119906833+ifeincarnation@users.noreply.github.com>
2022-12-28 08:36:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b banana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -15,7 +15,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka banana -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka banana -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
