7f6b60dff556fbfa653ec28a7059fdb9bd687682
IF Eincarnation <119906833+ifeincarnation@users.noreply.github.com>
2022-12-31 20:31:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b queso -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/bootleg-q/manifest.git -b queso -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b bootleg-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
