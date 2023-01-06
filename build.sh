3cdb3f1c21ec0a91ac44246f6b66339a42671ba6
IF Eincarnation <119906833+ifeincarnation@users.noreply.github.com>
2023-01-06 08:14:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# sync rom
+qq # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ifeincarnation/local_manifest --depth 1 -b nad-10-lavender .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -16,7 +16,7 @@ lunch nad_lavender-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
-progress & # Jangan di Hapus
+progress & # Jangan dig Hapus
 mka nad -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
